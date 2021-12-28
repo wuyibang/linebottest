481dec6570c1430e5401db0c311d89aaef4e540f
unknown <tyjh20804@gmail.com>
2021-12-29 02:48:04 +0800

    test robot

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def callback():
             continue
 
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
+            event.reply_token, TextSendMessage(text=event.message.text+"518 is handsome")
         )
 
     return "OK"
