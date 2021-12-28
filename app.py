f30f5bd2814a7424363b6483c8ea3a0575445f44
unknown <tyjh20804@gmail.com>
2021-12-28 21:54:51 +0800

    test 2

--- app.py
+++ app.py
@@ -111,8 +111,8 @@ def webhook_handler():
 
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
-    machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
+    #machine.get_graph().draw("fsm.png", prog="dot", format="png")
+    #return send_file("fsm.png", mimetype="image/png")
 
 
 if __name__ == "__main__":
