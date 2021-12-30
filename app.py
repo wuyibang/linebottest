5cabcbf7e39378e9660605694bea5935bd910999
unknown <tyjh20804@gmail.com>
2021-12-31 03:28:22 +0800

    test draw pic2

--- app.py
+++ app.py
@@ -125,5 +125,5 @@ def show_fsm():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=True,use_reloader=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
     print("PPPPPPPPPPP",port)
