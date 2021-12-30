965116d27ef040bdfd94ea006ef9a71beee994bb
unknown <tyjh20804@gmail.com>
2021-12-30 19:56:01 +0800

    test debug = true reloader = False

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ def show_fsm():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=False,use_reloader=False)
+    app.run(host="0.0.0.0", port=port, debug=True,use_reloader=False)
     print("PPPPPPPPPPP",port)
