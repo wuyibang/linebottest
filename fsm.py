f96b517215737cfd7abaa550b0b16a57e46ea9bf
unknown <tyjh20804@gmail.com>
2021-12-31 11:16:22 +0800

    try different state2

--- fsm.py
+++ fsm.py
@@ -22,12 +22,11 @@ class TocMachine(GraphMachine):
 
     def on_enter_state1(self, event):
         print("I'm entering state1")
-
         reply_token = event.reply_token
         send_text_message(reply_token, "Trigger state1")
 
-    def on_exit_state1(self):
-        print("Leaving state1")
+    # def on_exit_state1(self):
+    #     print("Leaving state1")
 
     def on_enter_state2(self, event):
         print("I'm entering state2")
@@ -36,8 +35,8 @@ class TocMachine(GraphMachine):
         send_text_message(reply_token, "Trigger state2")
         self.go_back()
 
-    def on_exit_state2(self):
-        print("Leaving state2")
+    # def on_exit_state2(self):
+    #     print("Leaving state2")
 
     def on_enter_user(self, event):
         print("I'm back")
