161968bc67a94786bf3af1e70c70a9312a935821
unknown <tyjh20804@gmail.com>
2021-12-31 19:30:03 +0800

    try menu

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateS
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
-
+line_bot_api = LineBotApi(channel_access_token)
 
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
