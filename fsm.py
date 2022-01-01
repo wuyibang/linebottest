d8dfd6d2d36e37af440d66cc1d32b65fd0dc68d1
unknown <tyjh20804@gmail.com>
2022-01-02 01:33:03 +0800

    add player_madia

--- fsm.py
+++ fsm.py
@@ -4,6 +4,8 @@ from utils import send_text_message, send_button_message, send_image_carousel
 #from webcrawler import searchplayer
 STARPLAYER=["林安可","蘇智傑","陳傑憲","林靖凱","林岱安","胡智為","陳韻文","古林睿煬"]
 Player_img={"林安可":"https://i.imgur.com/Lxf4h5q.png","蘇智傑":"https://i.imgur.com/u5t2HSq.png","陳傑憲":"https://i.imgur.com/QYPWtnm.png","林靖凱":"https://i.imgur.com/HaPr7f7.png","林岱安":"https://i.imgur.com/ErNTsIu.jpg","胡智為":"https://i.imgur.com/JmKFXB4.png","陳韻文":"https://i.imgur.com/pe63Sc1.png","古林睿煬":"https://i.imgur.com/fP0NVC4.png"}
+Player_pic=['https://i.imgur.com/p9f43YQ.png','https://i.imgur.com/Z93xq2s.png','https://i.imgur.com/p1Txo3R.png','https://i.imgur.com/RyVGii0.png','https://i.imgur.com/PFOPip5.png','https://i.imgur.com/ME2rd9v.png','https://i.imgur.com/KCHEFVA.png','https://i.imgur.com/PdsKGPy.png']
+Player_media=['https://www.facebook.com/ankolin1997','https://www.instagram.com/ccsu.32/','https://www.instagram.com/hsien_1994','https://www.instagram.com/c.k.lin_64_/','https://www.instagram.com/lin_dai.an168/','https://www.facebook.com/profile.php?id=100044237085864','https://www.instagram.com/lions_cyw12/','https://www.instagram.com/612_lao/']
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
@@ -97,12 +99,33 @@ class TocMachine(GraphMachine):
         print("IN TEAMLIONS")
         reply_token = event.reply_token
         name = event.message.text
-        img_url=Player_img[name]
-        title = "統一獅_球隊介紹"
-        uptext = "查看球隊社群及官方商城"
-        labels = ["IG","FB","YOUTUBE","LIONCREW"]
-        texts = ["IG","FB","YOUTUBE","LIONCREW"]
+        for i in range(len(STARPLAYER)):
+            if STARPLAYER[i] == name:
+                num = i
+                break
+        img_url = Player_pic[i]
+        title = name
+        uptext = "查看球員"
+        labels = ["社群","2021 數據","上一頁"]
+        texts = ["社群","2021 數據","上一頁"]
         send_button_message(reply_token,img_url,title,uptext,labels,texts)
+
+    def on_enter_playerinfo_media(self, event):
+        print("Show player media")
+        name = event.message.text
+        for i in range(len(STARPLAYER)):
+            if STARPLAYER[i] == name:
+                num = i
+                break
+        url = Player_media[i]
+        send_text_message(reply_token,url)
+
+    def on_enter_playerinfo_stat(self, event):
+        print("Show player media")
+        name = event.message.text
+
+        send_text_message(reply_token,url)
+
 
     # def on_enter_ShowHottestPlayer(self, event):
     #     print("Show hottest player")
