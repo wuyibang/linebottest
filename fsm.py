4af113d5175dd57dccc9eab709c0b443863a41f3
unknown <tyjh20804@gmail.com>
2022-01-02 02:46:24 +0800

    add player_stat3

--- fsm.py
+++ fsm.py
@@ -150,6 +150,11 @@ class TocMachine(GraphMachine):
         st = "IP:100\nW:8 K/9:9.54\nERA:3.15 WHIP:1.25\n2021 單季破百局百K"
         send_text_message(reply_token,st)
         self.go_back()
+    def on_exit_playerinfo_media(self):
+        print("Leaving media")
+    def on_exit_playerinfo_stat(self):
+        print("Leaving stat")
+
 
     # def on_enter_ShowHottestPlayer(self, event):
     #     print("Show hottest player")
